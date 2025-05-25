a374b150f231c0c79546a8c4ded70931cf231c27
maivuquoc <168701196+maivuquoc@users.noreply.github.com>
2025-05-25 15:55:58 +0700

    Fix Vercel 404: add base to vite.config.js

--- MaiVuQuocjsx-template/vite.config.js
+++ MaiVuQuocjsx-template/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 Dòng quan trọng để tránh lỗi 404 trên Vercel
 })
