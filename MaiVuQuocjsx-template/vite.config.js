5d8bdeaa46c601f9b7024eae73f279718a30ae0f
maivuquoc <168701196+maivuquoc@users.noreply.github.com>
2025-05-25 16:22:21 +0700

    Cập nhật vite.config.js và xóa vercel.json

--- MaiVuQuocjsx-template/vite.config.js
+++ MaiVuQuocjsx-template/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 Dòng quan trọng để tránh lỗi 404 trên Vercel
+  base: '/'
 })
